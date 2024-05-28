4d83a8c433fe3fe3444e9a81c99f0ba1c78fe496
shinjoonseo <wsx3815@mgmail.com>
2024-05-28 21:37:18 +0900

    1111

--- python/random_game.py
+++ python/random_game.py
@@ -4,11 +4,23 @@ class random_game :
 
   # 제비뽑기 함수
   def 제비뽑기(self) :
-    players_num = int(input("플레이할 인원을 입력해 주세요."))
+    while True:
+      players_num = input("플레이할 인원을 입력해 주세요.")
+      if players_num.isdigit() == False :
+        print("숫자를 입력해 주세요.")
+        continue
+      players_num = int(players_num)
+      break
     players = []
     for i in range(players_num) :
       players.append(input(f"플레이어{i+1} 이름을 입력해 주세요."))
-    choice_num = int(input("뽑기할 제비개수를 정해 주세요."))
+    while True:
+      choice_num = input("뽑기할 제비개수를 정해 주세요.")
+      if choice_num.isdigit() == False :
+        print("숫자를 입력해 주세요.")
+        continue
+      choice_num = int(choice_num)
+      break
     choice_list = []
     for i in range(choice_num) :
       choice_list.append(input("제비 내용을 입력해주세요."))
@@ -20,7 +32,13 @@ class random_game :
 
   # 룰렛
   def 룰렛(self) :
-    choice_num = int(input("룰렛 칸 개수를 입력해 주세요."))
+    while True:
+      choice_num = input("룰렛 칸 개수를 입력해 주세요.")
+      if choice_num.isdigit() == False :
+        print("숫자를 입력해 주세요.")
+        continue
+      choice_num = int(choice_num)
+      break
     choice_list = []
     for i in range(choice_num) :
       choice_list.append(input("룰렛 칸 내용을 입력해 주세요."))
